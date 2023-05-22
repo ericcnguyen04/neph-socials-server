64250c5849da30ca3c162e8a28bf6ed3749eca73
ericcnguyen04 <ericc.win@gmail.com>
2023-05-21 23:41:37 -0400

    authentication and hashing passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,3 +2,29 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import User from "../models/User.js"
 
+// Register User
+export const register = async (req, res) => {
+    try {
+        const {
+            userName,
+            email,
+            password,
+            picturePath,
+            friends
+        } = req.body
+
+        // securing password
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            userName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends
+        })
+    } catch (err) {
+
+    }
+}
