55735837016a92273a3871dd7dd69b110b5e1801
ericcnguyen04 <ericc.win@gmail.com>
2023-05-21 23:50:38 -0400

    added routes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url"; 
+import authRoutes from "./routes/auth.js"
 import {register} from "./controllers/auth.js"
  
 // configerations
@@ -38,6 +39,10 @@ const upload = multer({storage});
 
 // Routes with Files
 app.post("/auth/register", upload.single("picture"), register);
+
+// Routes
+app.use("/auth", authRoutes)
+
 
 // mongoose setup //time stamp: 17:28  // to test, use- nodemon index.js
 const PORT = process.env.PORT || 6001
